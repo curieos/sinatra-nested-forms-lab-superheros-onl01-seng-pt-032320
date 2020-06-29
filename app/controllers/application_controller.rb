c72966a6997853582c349696ce4cd68d3f08b47b
Caroline <cvrieous@gmail.com>
2020-06-29 10:33:10 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
+# frozen_string_literal: true
+
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  set :views, proc { File.join(root, '../views/') }
 
-    set :views, Proc.new { File.join(root, "../views/") }
+  get '/' do
+    erb :super_hero
+  end
 
+  post '/teams' do
+    @team = params[:team]
 
+    erb :team
+  end
 end
